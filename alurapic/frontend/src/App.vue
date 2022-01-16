e2a8fc959ed6f8443989599572db62e4355362f6
rubens-lavor <haryson07@gmail.com>
2022-01-16 15:48:34 -0300

    add router-view

--- alurapic/frontend/src/App.vue
+++ alurapic/frontend/src/App.vue
@@ -1,91 +1,33 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-
-    <input
-      type="search"
-      class="filtro"
-      v-on:input="filtro = $event.target.value"
-      placeholder="filtre pelo título da foto"
-    />
-
-    <ul class="lista-fotos">
-      <li
-        class="lista-fotos-item"
-        v-for="(foto, index) of fotosComFiltro"
-        :key="index"
-      >
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-        </meu-painel>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li v-for="route, index in routes" :key="index">
+          <router-link :to="route.path">
+            {{route.titulo}}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel.vue";
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
+import { routes } from './routes'
 
 export default {
-  components: {
-    "meu-painel": Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
-
   data() {
     return {
-      titulo: "Alurapic",
-
-      fotos: [],
-
-      filtro: "",
-    };
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter((foto) => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    },
-  },
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then((res) => res.json())
-      .then(
-        (fotos) => (this.fotos = fotos),
-        (err) => console.log(err)
-      );
-  },
+      routes
+    }
+  }
 };
 </script>
 <style>
-.centralizado {
-  text-align: center;
-}
-
 .corpo {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
-}
-
-.lista-fotos {
-  list-style: none;
-}
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
 }
 </style>
--- alurapic/frontend/src/main.js
+++ alurapic/frontend/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import { routes } from './routes';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
